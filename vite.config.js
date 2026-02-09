25ee7c9330fe6bf0d1a8dc4e8dbc9ba7434aea1e
sumitk-mridha <sumitk.mridha@gmail.com>
2026-02-09 00:50:08 -0500

    fix for file errors after deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ctrl-plus-c/',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
